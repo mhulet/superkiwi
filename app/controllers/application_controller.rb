37022389d0d17fda4db1ce441a458c00143f9117
Michael Hulet <michael@hulet.eu>
2017-07-27 22:02:24 +0200

    Add basic HTTP authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_AUTH_USERNAME'] && password == ENV['HTTP_AUTH_PASSWORD']
+      end
+    end
+  end
 end
