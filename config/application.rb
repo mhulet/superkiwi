ba54a7875e78df760e9e06092ca9cbb04b6c9f4b
Michael Hulet <michael@hulet.eu>
2017-07-28 20:54:02 +0200

    Set Sidekiq as ActiveJob queue adapter

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Superkiwi
     config.i18n.available_locales = [:fr]
     config.i18n.default_locale = :fr
 
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
